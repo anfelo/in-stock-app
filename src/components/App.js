8e6c7c3349e0b89375c42748e1fd52dfcb65b631
anfelo <af.osorio1341@gmail.com>
2017-08-17 21:06:54 -0500

    Testing with D3 and React rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from '../assets/img/logo.svg';
 import '../assets/styles/App.css';
 
+import { scaleLinear } from 'd3-scale';
+import { scaleTime } from 'd3-scale';
+import { line } from 'd3-shape';
+import { curveCardinal } from 'd3-shape';
+import * as d3 from "d3";
+import { timeFormat } from 'd3-time-format';
+import { timeParse } from 'd3-time-format';
+
 class SearchForm extends Component {
 
   constructor() {
@@ -52,14 +60,75 @@ const Footer = () => {
 }
 
 class Chart extends Component {
+  constructor(props){
+     super(props)
+     this.createBarChart = this.createBarChart.bind(this)
+  };
 
+  componentDidMount = () => {
+     this.createBarChart()
+  };
+
+  componentDidUpdate = () => {
+     this.createBarChart()
+  };
+
+  createBarChart = () => {
+  };
+
+  render() {
+
+    const margin = {top: 5, right: 50, bottom: 20, left: 50},
+    width = this.props.width - (margin.left + margin.right),
+    height = this.props.height - (margin.top + margin.bottom);
+
+    const parseDate = timeParse("%m-%d-%Y");
+
+    const data = this.props.data;
+
+    data.forEach(function (d) {
+        d.date = parseDate(d.day);
+    });
+
+    const x = scaleTime()
+    .domain(d3.extent(data, function (d) {
+        return d.date;
+    }))
+    .rangeRound([0, width]);
+
+    const y = scaleLinear()
+    .domain([0,d3.max(data,function(d){
+        return d.count+100;
+    })])
+    .range([height, 0]);
+
+    const chartLine = line()
+    .x(function (d) {
+        return x(d.date);
+    })
+    .y(function (d) {
+        return y(d.count);
+    }).curve(curveCardinal);
+
+    const transform='translate(' + margin.left + ',' + margin.top + ')';
+
+    return (
+      <div>
+          <svg width={this.props.width} height={this.props.height}>
+              <g transform={transform}>
+                  <path className="line shadow" d={chartLine(data)} strokeLinecap="round"/>
+              </g>
+          </svg>
+      </div>
+    );
+  }
 }
 
 class ChartContainer extends Component {
   render() {
     return (
       <div className="chart-container">
-        <Chart />
+        <Chart data={this.props.data} size={[800,500]}/>
       </div>
     );
   }
@@ -86,12 +155,27 @@ const StocksContianer = props => {
 }
 
 class App extends Component {
+
+  state = {
+		data: [
+        {day:'02-11-2016',count:180},
+        {day:'02-12-2016',count:250},
+        {day:'02-13-2016',count:150},
+        {day:'02-14-2016',count:496},
+        {day:'02-15-2016',count:140},
+        {day:'02-16-2016',count:380},
+        {day:'02-17-2016',count:100},
+        {day:'02-18-2016',count:150}
+    ]
+	};
+
+
   render() {
     return (
       <div className="App">
         <Header />
         <div className="App-main-content">
-          <ChartContainer />
+          <ChartContainer data={this.state.data}/>
           <StocksContianer />
         </div>
         <Footer />
