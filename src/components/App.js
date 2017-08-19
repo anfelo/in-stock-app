8cc4b07096b6cb5aa5e4708a8dbb98f01ac248a9
anfelo <af.osorio1341@gmail.com>
2017-08-18 19:12:01 -0500

    Added multiline chart functionality plus chart style

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from '../assets/img/logo.svg';
 import '../assets/styles/App.css';
 
 import { line } from 'd3-shape';
 import { scaleLinear } from 'd3-scale';
 import { scaleTime } from 'd3-scale';
-import { timeFormat, timeParse } from 'd3-time-format';
+import { timeParse } from 'd3-time-format';
 import { extent, max } from 'd3-array';
 import { select } from 'd3-selection';
+import { axisLeft, axisBottom } from 'd3-axis';
 
 class SearchForm extends Component {
 
@@ -58,71 +60,75 @@ const Footer = () => {
   );
 }
 
+class Axis extends Component {
+  componentDidMount (){
+    this.renderAxis();
+  }
+
+  componentDidUpdate () {
+    this.renderAxis();
+  }
+
+  renderAxis () {
+    const node = ReactDOM.findDOMNode(this);
+    select(node).call(this.props.axis);
+  }
+
+  render () {
+    const translate = "translate(0,"+(this.props.height)+")";
+    return (
+        <g className="axis" transform={this.props.axisType==='x'?translate:""} >
+        </g>
+    );
+  }
+}
+
+class Grid extends Component {
+  componentDidMount (){
+    this.renderGrid();
+  }
+
+  componentDidUpdate () {
+    this.renderGrid();
+  }
+
+  renderGrid () {
+    const node = ReactDOM.findDOMNode(this);
+    select(node).call(this.props.grid);
+  }
+
+  render () {
+    var translate = "translate(0,"+(this.props.height)+")";
+    return (
+        <g className="y-grid" transform={this.props.gridType==='x'?translate:""}>
+        </g>
+    );
+  }
+}
+
 class Chart extends Component {
-  constructor(props){
-     super(props)
-     this.createBarChart = this.createBarChart.bind(this)
-  };
-
-  componentDidMount = () => {
-     this.createBarChart()
-  };
-
-  componentDidUpdate = () => {
-     this.createBarChart()
-  };
-
-  createBarChart = () => {
-    // const margin = {top: 20, right: 50, bottom: 20, left: 50},
-    //       width = this.props.size[0] - (margin.left + margin.right),
-    //       height = this.props.size[1] - (margin.top + margin.bottom);
-
-    // const node = this.node;
-    // const parseDate = timeFormat("%m-%d-%Y");
-
-    // const data = this.props.data;
-    // data.forEach(function (d) {
-    //   d.date = parseDate(d.day);
-    // });
-
-    // const dataMax = max(data, function(d){
-    //   return d.count;
-    // });
-
-    // const yScale = scaleLinear()
-    //   .domain([0, dataMax])
-    //   .range([height, 0]);
-
-    // const xScale = scaleTime()
-    //   .domain(extent(data, function(d){
-    //     return d.date;
-    //   }))
-    //   .rangeRound([0, width]);
-
-    // var stockLine = line(data)
-    //     .x(function (d) {
-    //         return xScale(d.date);
-    //     })
-    //     .y(function (d) {
-    //         return yScale(d.count);
-    //     });
-  };
-
   render() {
-    const margin = {top: 20, right: 50, bottom: 20, left: 50},
+    const colors = ['#7dc7f4','#c25975','#53bbb4','#f9845b','#51b46d'];
+    const margin = {top: 40, right: 50, bottom: 60, left: 70},
     width = this.props.size[0] - (margin.left + margin.right),
     height = this.props.size[1] - (margin.top + margin.bottom);
 
-    const node = this.node;
     const parseDate = timeParse("%m-%d-%Y");
 
     const data = this.props.data;
-    data.forEach(function (d) {
-    d.date = parseDate(d.day);
+    data.forEach(function (stock) {
+      stock.data.forEach(function (d){
+        d.date = parseDate(d.day);
+      });
     });
 
-    const dataMax = max(data, function(d){
-    return d.count;
+    let allValues = [];
+    data.forEach(function (stock) {
+      allValues = allValues.concat(stock.data);
+    });
+
+    const dataMax = max(allValues, function(d){
+      return d.count + 100;
     });
 
     const yScale = scaleLinear()
@@ -130,12 +136,30 @@ class Chart extends Component {
     .range([height, 0]);
 
     const xScale = scaleTime()
-    .domain(extent(data, function(d){
+    .domain(extent(data[0].data, function(d){
       return d.date;
     }))
     .rangeRound([0, width]);
 
-    var stockLine = line()
+    const yAxis = axisLeft()
+    .scale(yScale)
+    .ticks(5);
+
+    const xAxis = axisBottom()
+    .scale(xScale)
+    // eslint-disable-next-line
+    .tickValues(data[0].data.map(function(d,i){
+      if(i>0) return d.date;
+    }).splice(1))
+    .ticks(4);
+
+    const yGrid = axisLeft()
+    .scale(yScale)
+    .ticks(5)
+    .tickSize(-width, 0, 0)
+    .tickFormat("");
+
+    const stockLine = line()
     .x(function (d) {
         return xScale(d.date);
     })
@@ -143,9 +167,20 @@ class Chart extends Component {
         return yScale(d.count);
     });
 
+    const transform ='translate(' + margin.left + ',' + margin.top + ')';
+
+    const lines = data.map(function(stock, index){
+      return <path key={index + 1} className="line shadow" stroke={colors[index]} d={stockLine(stock.data)} strokeLinecap="round"/>
+    });
+
     return (
-      <svg ref={node => this.node = node} width={this.props.size[0]} height={this.props.size[1]}> 
-        <path className="line shadow" d={stockLine(data)} strokeLinecap="round"/>
+      <svg width={this.props.size[0]} height={this.props.size[1]}> 
+        <g transform={transform}>
+          <Grid height={height} grid={yGrid} gridType="y"/>
+          <Axis height={height} axis={yAxis} axisType="y"/>
+          <Axis height={height} axis={xAxis} axisType="x"/>
+          { lines }
+        </g>
       </svg>
     );
   }
@@ -155,7 +190,7 @@ class ChartContainer extends Component {
   render() {
     return (
       <div className="chart-container">
-        <Chart data={this.props.data} size={[800,500]}/>
+        <Chart data={this.props.data} size={[1100,500]}/>
       </div>
     );
   }
@@ -184,18 +219,48 @@ const StocksContianer = props => {
 class App extends Component {
 
   state = {
-		data: [
-      {day:'02-11-2016',count:180},
-      {day:'02-12-2016',count:250},
-      {day:'02-13-2016',count:150},
-      {day:'02-14-2016',count:496},
-      {day:'02-15-2016',count:140},
-      {day:'02-16-2016',count:380},
-      {day:'02-17-2016',count:100},
-      {day:'02-18-2016',count:150}
+		data: [ 
+      {
+        name:'test1',
+        data:[
+          {day:'02-11-2016',count:180},
+          {day:'02-12-2016',count:250},
+          {day:'02-13-2016',count:150},
+          {day:'02-14-2016',count:496},
+          {day:'02-15-2016',count:140},
+          {day:'02-16-2016',count:380},
+          {day:'02-17-2016',count:100},
+          {day:'02-18-2016',count:150}
+        ]
+      },
+      {
+        name:'test2',
+        data:[
+          {day:'02-11-2016',count:60},
+          {day:'02-12-2016',count:110},
+          {day:'02-13-2016',count:80},
+          {day:'02-14-2016',count:89},
+          {day:'02-15-2016',count:50},
+          {day:'02-16-2016',count:10},
+          {day:'02-17-2016',count:50},
+          {day:'02-18-2016',count:70}
+        ]
+      },
+      {
+        name:'test3',
+        data:[
+          {day:'02-11-2016',count:400},
+          {day:'02-12-2016',count:550},
+          {day:'02-13-2016',count:500},
+          {day:'02-14-2016',count:300},
+          {day:'02-15-2016',count:380},
+          {day:'02-16-2016',count:400},
+          {day:'02-17-2016',count:460},
+          {day:'02-18-2016',count:300}
+        ]
+      },
     ]
 	};
-
 
   render() {
     return (
